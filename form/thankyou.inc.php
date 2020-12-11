4391190f6af0b54b88570c465520478c0b6cd45c
Luis Pacheco <luis.pachecourbina@gmail.com>
2020-12-11 16:27:10 -0500

    init commit

---
+++ form/thankyou.inc.php
@@ -0,0 +1,77 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: sans-serif;
+}
+body{
+  background: url('../images/background.jpg') #374461;
+  width: 1350px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+}
+.container{
+  max-width: 1350px;
+  margin: 50px auto 80px;
+  text-align: center;
+  color: white;
+}
+
+nav ul li{
+  display: inline;
+}
+
+nav a{
+  text-decoration: underline;
+  color: white;
+  background: none;
+  padding: 35px;
+  border-radius: 3px;
+  font-weight: bold;
+}
+nav a:link{
+  text-decoration: underline;
+}
+nav a:visited{
+ text-decoration: line-through;
+}
+
+header{
+  text-align: center;
+  padding: 20px 0
+}
+
+footer{
+  color: white;
+  background: gray;
+  padding: 20px 0;
+  text-align: center;
+}
+footer::first-line {
+  color: white;
+  text-transform: uppercase;
+  font-weight: bolder;
+}
+</style>
+
+<header>
+  <nav>
+    <ul>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="contact.html">Contact Me</a></li>
+    </ul>
+  </nav>
+</header>
+<div class="container">
+  <h1>Thank you for your enquiry</h1>
+  <p>Thanks for your interest.I wil be in touch with you shortly</p>
+</div>
+
+
+<footer>
+  Portfolio by Luis Pacheco
+</footer>
